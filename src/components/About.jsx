bc811c05e6c36d12f085ef0230c7a210ee374aca
Siddharth Paudel <psiddhartha62@gmail.com>
2026-05-13 11:31:10 +0545

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
     {
       school: "Valmiki Shiksha Sadan",
       logo: "/valmiki.png",
-      degree: "Grade 12 · Science",
+      degree: "Grade 12 ·  Science Stream",
     },
   ];
 
